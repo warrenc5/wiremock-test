b3d3200286dc6cddef724015ea78aac41ee82568
Warren Crossing <warren@itcl.com.au>
2025-04-19 08:44:04 +1000

    rebuild docker with mvn dependency

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
+mvn 
+docker buildx build . --progress=plain 
 docker run --network host -p 8080:8080 -v .:/app amazoncorretto:11-alpine3.21-jdk java -jar app/target/dependency/wiremock-standalone-3.12.1.jar --root-dir /app --global-response-templating --verbose true
