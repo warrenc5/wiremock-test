416f9ec97cf9ce4e45c0de4dd3090c0b2658df79
Warren Crossing <warren@itcl.com.au>
2025-07-27 11:26:59 +1000

    update for 4.0.0

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-mvn 
+#mvn 
+cp -v /home/wozza/code/wiremock2/build/libs/wiremock-standalone*.jar ./target/dependency/
 docker buildx build . --progress=plain 
-docker run --network host -p 8080:8080 -v .:/app amazoncorretto:11-alpine3.21-jdk java -jar app/target/dependency/wiremock-standalone-3.12.1.jar --root-dir /app --global-response-templating --verbose true
+docker run --network host -p 8080:8080 -v .:/app amazoncorretto:17-alpine-jdk java -jar app/target/dependency/wiremock-standalone-4.0.0-beta.14.jar --root-dir /app --global-response-templating --verbose true
--- test.sh
+++ test.sh
@@ -1,2 +1,4 @@
 #curl -v http://localhost:8080/hello
+curl -v -X POST http://localhost:8080/__admin/reset
 curl -v -d @__files/primary-request.json http://localhost:8080/primary
+curl -v -d @__files/primary-request.json http://localhost:8080/primary-serveevent
