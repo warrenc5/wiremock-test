4c2d854fdc91e9c2c9042155151a5aa07ecd8245
Warren Crossing <warren@itcl.com.au>
2025-12-19 21:24:54 +1100

    fix tests with better assertions

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
-#mvn 
-cp -v /home/wozza/code/wiremock2/build/libs/wiremock-standalone*.jar ./target/dependency/
-docker buildx build . --progress=plain 
-docker run --network host -p 8080:8080 -v .:/app amazoncorretto:17-alpine-jdk java -jar app/target/dependency/wiremock-standalone-4.0.0-beta.14.jar --root-dir /app --global-response-templating --verbose true
+#mvn
+
+cp -v /home/wozza/code/wiremock4/build/libs/wiremock-standalone*.jar ./target/dependency/
+docker rm -f wiremock-test
+docker buildx build . --progress=plain
+docker run --name wiremock-test --network host -p 8080:8080 -v .:/app amazoncorretto:17-alpine-jdk java -jar app/target/dependency/$(basename target/dependency/wiremock-standalone*) --root-dir /app --global-response-templating --verbose true &
+
+#test.sh
--- test.sh
+++ test.sh
@@ -1,4 +1,23 @@
+#!/bin/bash
+set -Eeuo pipefail
+reset
+BLUE='\e[34m'
+RED='\e[31m'
+GREEN='\e[32m'
+CYAN='\e[36m'
+MAGENTA='\e[35m'
+RESET='\e[0m'
+
+err() {
+    echo -e "${RED}There was an error${RESET}" >&2;
+}
+
+trap 'err; exit 1' ERR
+echo -e $BLUE
 #curl -v http://localhost:8080/hello
-curl -v -X POST http://localhost:8080/__admin/reset
-curl -v -d @__files/primary-request.json http://localhost:8080/primary
-curl -v -d @__files/primary-request.json http://localhost:8080/primary-serveevent
+
+curl -v -X POST http://localhost:8080/__admin/reset && echo -e "${GREEN} reset completed${RESET}"
+echo -e $CYAN
+curl -v -d @__files/primary-request-postserve.json http://localhost:8080/primary-postserve | tee /dev/stderr | jq -e '. == {"primary-response-primary-request":"postserve","primary-response":"WOW-postserve"}' &&  echo -e "${GREEN}POST_SERVE primary response passed${RESET}" || echo -e "${RED}POST_SERVE primary response FAILED${RESET}"
+echo -e $MAGENTA
+curl -v -d @__files/primary-request-serveevent.json http://localhost:8080/primary-serveevent | tee /dev/stderr | jq -e '. == {"primary-response-primary-request":"serveevent","primary-response":"WOW-serveevent"}' &&  echo -e "${GREEN}SERVE_EVENT primary response passed${RESET}" || echo -e "${RED}SERVE_EVENT primary response FAILED${RESET}"
